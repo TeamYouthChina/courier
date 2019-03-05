7b48b2c3ca166f95613ea740b7eb18bf9bfbd38c
JinhaoHu <hujinhao0401@gmail.com>
2019-03-05 09:54:00 -0500

    rabbitmq

--- src/main/java/com/youthchina/courier/service/EmailRequestReceiver.java
+++ src/main/java/com/youthchina/courier/service/EmailRequestReceiver.java
@@ -2,17 +2,46 @@ package com.youthchina.courier.service;
 
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 /**
  * Created by zhongyangwu on 2/21/19.
  */
 @Component
 @RabbitListener(queues = "hello")
-public class EmailRequestReceiver {
+public class EmailRequestReceiver extends MessageListenerAdapter {
 
     @RabbitHandler
-    public void onReceived(String message) {
-
+    public void onReceived(String m) throws IOException {
+        System.out.println("m");
+//        byte[] bytes = toByteArray(o);
+//
+//        File file2 = new File("/Users/dreamer/Documents/6234/NFLB.pdf");
+//        InputStream inputStream = new FileInputStream(file2);
+//        byte[] filestring = new byte[inputStream.available()];
+//
+//        if(Arrays.equals(filestring,bytes)){
+//            System.out.println("!!!!");
+//        }
+    }
+    public byte[] toByteArray (Object obj) {
+        byte[] bytes = null;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(obj);
+            oos.flush();
+            bytes = bos.toByteArray ();
+            oos.close();
+            bos.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return bytes;
     }
 }
--- src/test/java/com/youthchina/courier/CourierApplicationTests.java
+++ src/test/java/com/youthchina/courier/CourierApplicationTests.java
@@ -1,16 +1,46 @@
 package com.youthchina.courier;
 
+import com.youthchina.courier.service.EmailSender;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CourierApplicationTests {
+    @Autowired
+    private EmailSender emailSender;
 
+    @Autowired
+    RabbitTemplate rabbitTemplate;
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws IOException, ClassNotFoundException {
+//        File file = new File("/Users/dreamer/Documents/Resume/Jinhao_resume.pdf");
+//        InputStream inputStream = new FileInputStream(file);
+//        byte[] filestring = new byte[inputStream.available()];
+//        m.put("to",(Object)filestring);
+        emailSender.send("222");
     }
 
+//    public Object toObject (byte[] bytes) {
+//        Object obj = null;
+//        try {
+//            DatagramPacket packet = new DatagramPacket(bytes, bytes.length);
+//            ByteArrayInputStream bis = new ByteArrayInputStream (packet.getData());
+//            ObjectInputStream ois = new ObjectInputStream (bis);
+//            obj = ois.readObject();
+//            ois.close();
+//            bis.close();
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        } catch (ClassNotFoundException ex) {
+//            ex.printStackTrace();
+//        }
+//        return obj;
+//    }
 }
