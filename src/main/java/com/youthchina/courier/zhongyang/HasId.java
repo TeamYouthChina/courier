ad4a3deddeac3e2cf1f47767146da3f811e9eca5
wangqinghong <923793518@qq.com>
2019-03-05 14:21:34 -0500

    Merge branch 'master' of https://github.com/TeamYouthChina/courier
    # Conflicts:
    #	pom.xml

--- src/main/java/com/youthchina/courier/zhongyang/HasId.java
+++ src/main/java/com/youthchina/courier/zhongyang/HasId.java
@@ -1,8 +1,8 @@
-package com.youthchina.courier.zhongyang;
-
-/**
- * Created by zhongyangwu on 11/21/18.
- */
-public interface HasId<K> {
-    K getId();
-}
+//package com.youthchina.courier.zhongyang;
+//
+///**
+// * Created by zhongyangwu on 11/21/18.
+// */
+//public interface HasId<K> {
+//    K getId();
+//}
