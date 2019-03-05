6bd7b540f2d208833bbe368eb32a53010d860247
JinhaoHu <hujinhao0401@gmail.com>
2019-03-05 10:09:06 -0500

    rabbitmq

--- src/main/java/com/youthchina/courier/config/RabbitConfig.java
+++ src/main/java/com/youthchina/courier/config/RabbitConfig.java
@@ -4,15 +4,16 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 
 @Configuration
 @ComponentScan(basePackages = {"com.youthchina.courier"})
-//@PropertySource(value = {"classpath:application.properties"})
+@PropertySource(value = {"classpath:application.properties"})
 public class RabbitConfig {
     @Bean
     public Queue hello(){
-        return new Queue("hello");
+        return new Queue("hello",true);
     }
 }
 
